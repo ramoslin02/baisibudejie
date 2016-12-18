a7927fa95ed875e1ee0071e25583adc1cafd2e97
mark <wl1542997454@sina.com>
2016-12-18 20:35:25 +0800

    add comment to the .md file

--- pages/image/img.js
+++ pages/image/img.js
@@ -1,19 +1,36 @@
-// pages/image/img.js
+// pages/index/index.js
+var api = require('../../utils/api.js'); 
+
 Page({
-  data:{},
-  onLoad:function(options){
-    // 页面初始化 options为页面跳转所带来的参数
+  data:{
+    lists: []
   },
-  onReady:function(){
-    // 页面渲染完成
-  },
-  onShow:function(){
-    // 页面显示
-  },
-  onHide:function(){
-    // 页面隐藏
-  },
-  onUnload:function(){
-    // 页面关闭
+  onLoad:function() {
+    //console.log(api.getListUrl());
+    this.fetchData(10);
   }
+  ,
+  fetchData:function(type) {
+    var self = this;
+    wx.request({
+      url: api.getListUrl(type),
+      data: {},
+      method: 'GET', // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT
+      // header: {}, // 设置请求的 header
+      success: function(res){
+        self.setData({
+          lists: self.data.lists.concat(res.data.list)
+        })
+        // success
+        // console.log(self.data.lists);
+      },
+      fail: function() {
+        // fail
+      },
+      complete: function() {
+        // complete
+      }
+    })
+  }
+  
 })
